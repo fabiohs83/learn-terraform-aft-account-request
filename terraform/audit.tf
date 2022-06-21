1805f742cec5545f679bccf3fba42cd166140693
code-commit-at-121943979533 <fahbio@gmail.com>
2022-06-21 15:27:11 -0300

    audit account creation

---
+++ terraform/audit.tf
@@ -0,0 +1,27 @@
+module "audit" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "fahbio+audit@gmail.com"
+    AccountName               = "Audit"
+    ManagedOrganizationalUnit = "Security"
+    SSOUserEmail              = "fahbio+audit@gmail.com"
+    SSOUserFirstName          = "Audit"
+    SSOUserLastName           = "Audit"
+  }
+
+  account_tags = {
+    "Learn Tutorial" = "AFT"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "Fabio Henrique"
+    change_reason       = "Imported Audit Account into AFT"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  # account_customizations_name = "sandbox"
+}
